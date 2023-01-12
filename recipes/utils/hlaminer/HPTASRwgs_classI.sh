304aea9ca569508c506e3f19cfcc113ecb3b5556
Vishal Koparde <vishal.koparde@nih.gov>
2023-01-12 18:06:43 +0000

    updates

---
+++ recipes/utils/hlaminer/HPTASRwgs_classI.sh
@@ -0,0 +1,24 @@
+###Run TASR
+echo "Running TASR..."
+#TASR Default is -k 15 for recruiting reads. You may increase k, as long as k < L/2 where L is the minimum shotgun read length
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/TASR -f patient.fof -m 20 -k 20 -s /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_GEN.fasta -i 1 -b TASRhla -w 1
+###Restrict 200nt+ contigs
+cat TASRhla.contigs |perl -ne 'if(/size(\d+)/){if($1>=200){$flag=1;print;}else{$flag=0;}}else{print if($flag);}' > TASRhla200.contigs
+if [ -s TASRhla200.contigs ];
+then
+    echo "contigs with >200bp length are present"
+else
+    touch HLAminer_HPTASR.csv  && exit 0
+fi
+###Create a [NCBI] blastable database
+echo "Formatting blastable database..."
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/formatdb -p F -i TASRhla200.contigs
+###Align contigs against database
+echo "Aligning TASR contigs to HLA references..."
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/parseXMLblast.pl -c /opt2/HLAminer-1.4/HLAminer_v1.4/bin/ncbiBlastConfig.txt -d /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_GEN.fasta -i TASRhla200.contigs -o 0 -a 1 > tig_vs_hla-ncbi.coord
+###Align HLA references to contigs
+echo "Aligning HLA references to TASR contigs (go have a coffee, it may take a while)..."
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/parseXMLblast.pl -c /opt2/HLAminer-1.4/HLAminer_v1.4/bin/ncbiBlastConfig.txt -i /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_GEN.fasta -d TASRhla200.contigs -o 0 > hla_vs_tig-ncbi.coord
+###Predict HLA alleles
+echo "Predicting HLA alleles..."
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/HLAminer.pl -b tig_vs_hla-ncbi.coord -r hla_vs_tig-ncbi.coord -c TASRhla200.contigs -h /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_GEN.fasta
