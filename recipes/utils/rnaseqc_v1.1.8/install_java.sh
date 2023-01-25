7362911baa945ba09448852385234a9e7544e0e5
Vishal Koparde <vishal.koparde@nih.gov>
2023-01-25 16:03:36 +0000

    updates to rnaseqc v 1.1.8

--- recipes/utils/rnaseqc_v1.1.8/install_java.sh
+++ recipes/utils/rnaseqc_v1.1.8/install_java.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-source "/root/.sdkman/bin/sdkman-init.sh"
+#source "/root/.sdkman/bin/sdkman-init.sh"
+source "/opt2/sdk/bin/sdkman-init.sh"
+#if [ ! -d /opt2/java ];then mkdir -p /opt2/java;fi
+#sdk install java 7.0.352-zulu /opt2/java
 sdk install java 7.0.352-zulu
