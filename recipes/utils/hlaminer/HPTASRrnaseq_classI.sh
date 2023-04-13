95a4786c69abac3e8d6a739b4ee8f50154992e18
kopardev <vishal.koparde@nih.gov>
2023-04-13 14:21:50 -0400

    new version provided by VG see https://github.com/CCRGeneticsBranch/vineela_log/issues/83 for details

--- recipes/utils/hlaminer/HPTASRrnaseq_classI.sh
+++ recipes/utils/hlaminer/HPTASRrnaseq_classI.sh
@@ -15,10 +15,10 @@ echo "Formatting blastable database..."
 /opt2/HLAminer-1.4/HLAminer_v1.4/bin/formatdb -p F -i TASRhla200.contigs
 ###Align HLA contigs to references
 echo "Aligning TASR contigs to HLA references..."
-/opt2/HLAminer-1.4/HLAminer_v1.4/bin/parseXMLblast.pl -c ncbiBlastConfigO.txt -d /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_CDS.fasta -i TASRhla200.contigs -o 0 -a 1 > tig_vs_hla-ncbi.coord
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/parseXMLblast.pl -c /opt2/HLAminer-1.4/HLAminer_v1.4/bin/ncbiBlastConfigO.txt -d /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_CDS.fasta -i TASRhla200.contigs -o 0 -a 1 > tig_vs_hla-ncbi.coord
 ###Align HLA references to contigs
 echo "Aligning HLA references to TASR contigs (go have a coffee, it may take a while)..."
-/opt2/HLAminer-1.4/HLAminer_v1.4/bin/parseXMLblast.pl -c ncbiBlastConfigO.txt -i /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_CDS.fasta -d TASRhla200.contigs -o 0 > hla_vs_tig-ncbi.coord
+/opt2/HLAminer-1.4/HLAminer_v1.4/bin/parseXMLblast.pl -c /opt2/HLAminer-1.4/HLAminer_v1.4/bin/ncbiBlastConfigO.txt -i /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_CDS.fasta -d TASRhla200.contigs -o 0 > hla_vs_tig-ncbi.coord
 ###Predict HLA alleles
 echo "Predicting HLA alleles..."
 /opt2/HLAminer-1.4/HLAminer_v1.4/bin/HLAminer.pl -b tig_vs_hla-ncbi.coord -r hla_vs_tig-ncbi.coord -c TASRhla200.contigs -h /opt2/HLAminer-1.4/HLAminer_v1.4/database/HLA_ABC_CDS.fasta
